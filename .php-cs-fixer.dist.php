294bb7023b1e3cdce367bb6cd7e26aa424c978d8
Vidocq17 <AvidanB1704@gmail.com>
2026-02-05 12:40:18 +0100

    test: fix phpcsfixer excludes lint for linting

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -3,7 +3,9 @@
 $finder = (new PhpCsFixer\Finder())
     ->in(__DIR__)
     ->exclude('var')
-;
+    ->exclude('vendor')
+    ->notPath('public/index.php')
+    ->notPath('config/preload.php');
 
 return (new PhpCsFixer\Config())
     ->setRules([
